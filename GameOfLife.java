a8f6de4f51d228d050be8f0e03edd6240f6e539b
sdesaih <samarthdesain@gmail.com>
2024-12-26 21:43:43 +0530

    success

--- GameOfLife.java
+++ GameOfLife.java
@@ -1,19 +1,48 @@
 public class GameOfLife {
 
-  public static void gameOfLife(int[][] x) {
-    int row = x.length;
-    int col = x[0].length;
+  public static void gameOfLife(int[][] board) {
+    if (board == null || board.length == 0)
+      return;
 
-    for (int i = 0; i < row; i++) {
+    int m = board.length;
+    int n = board[0].length;
 
-      for (int j = 0; j < col; j++) {
+    int[][] copyBoard = new int[m][n];
 
-       
-         
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        copyBoard[i][j] = board[i][j];
       }
     }
 
-    
+    /** Direction array to gey all 8 directions */
+    int[] neighbours = { -1, 0, 1 };
+
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+
+        int liveNeighbours = 0;
+
+        /** Check for all neoghbours */
+        for (int x : neighbours) {
+          for (int y : neighbours) {
+
+            if (!(x == 0 && y == 0)) {
+              int r = i + x;
+              int c = j + y;
+
+              if ((r >= 0 && r < m) && (c >= 0 && c < n) && copyBoard[r][c] == 1)
+                liveNeighbours++;
+            }
+          }
+        }
+
+        if ((copyBoard[i][j] == 1) && (liveNeighbours < 2 || liveNeighbours > 3))
+          board[i][j] = 0;
+        if ((copyBoard[i][j] == 0) && liveNeighbours == 3)
+          board[i][j] = 1;
+      }
+    }
   }
 
   public static void main(String[] args) {
@@ -25,5 +54,12 @@ public class GameOfLife {
     };
 
     gameOfLife(matrix);
+
+    for (int[] x : matrix) {
+      for (int xDash : x) {
+        System.out.print(xDash + " ");
+      }
+      System.out.println();
+    }
   }
 }
