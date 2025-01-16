284c3580b97745913ce621d34c1c0f7ed68624c4
sdesaih <samarthdesain@gmail.com>
2025-01-16 10:23:35 +0530

    success

--- GetMax.java
+++ GetMax.java
@@ -1,24 +1,39 @@
-public class GetMax {
+class Array {
 
-  public static void getMax(int[] nums) {
+  public int Max(int[] nums) {
     int max = Integer.MIN_VALUE;
-    int index = -1;
 
     for (int i = 0; i < nums.length; i++) {
       if (nums[i] > max) {
         max = nums[i];
-        index = i;
       }
     }
 
-    System.out.println(max);
-    System.out.println(index);
+    return max;
   }
+
+  public int Min(int[] nums) {
+    int min = Integer.MAX_VALUE;
+
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] < min) {
+        min = nums[i];
+      }
+    }
+
+    return min;
+  }
+
+}
+
+public class GetMax {
 
   public static void main(String[] args) {
     int[] nums = { 2, 8, 5, 6, 3, 2, 5, 70 };
 
-    getMax(nums);
+    Array foo = new Array();
 
+    System.out.println(foo.Max(nums));
+    System.out.println(foo.Min(nums));
   }
 }
