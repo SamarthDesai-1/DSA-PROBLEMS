50edacf1541b921bb4e853b7836ce4a86bf45dc3
sdesaih <samarthdesain@gmail.com>
2025-01-16 18:05:04 +0530

    success

---
+++ AlternatingDigitSum.java
@@ -0,0 +1,41 @@
+public class AlternatingDigitSum {
+
+  /**
+   * @param n the number to compute the size of
+   * @return the number of digits in n
+   */
+  public static int size(int n) {
+    int size = 0;
+
+    while (n > 0) {
+      size++;
+      n /= 10;
+    }
+
+    return size;
+  }
+
+  public static int minimumOperations(int n) {
+    int size = size(n);
+
+    int sum = 0;
+
+    while (n > 0) {
+      int lastDigit = n % 10;
+
+      sum = sum + (size % 2 == 0 ? lastDigit * -1 : lastDigit);
+
+      size--;
+
+      n /= 10;
+    }
+
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    int n = 886996;
+
+    System.out.println("Alternating digit sum is : " + minimumOperations(n));
+  }
+}
