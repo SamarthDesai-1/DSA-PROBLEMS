d0ab655fdb66c810903651096e368969b97fa932
sdesaih <samarthdesain@gmail.com>
2025-01-16 18:06:21 +0530

    success

--- AlternatingDigitSum.java
+++ AlternatingDigitSum.java
@@ -4,7 +4,7 @@ public class AlternatingDigitSum {
    * @param n the number to compute the size of
    * @return the number of digits in n
    */
-  
+
   public static int size(int n) {
     int size = 0;
 
@@ -20,6 +20,7 @@ public class AlternatingDigitSum {
    * @param n the number to compute the alternating digit sum of
    * @return the alternating digit sum of n
    */
+  
   public static int minimumOperations(int n) {
     int size = size(n);
 
@@ -38,6 +39,15 @@ public class AlternatingDigitSum {
     return sum;
   }
 
+  /**
+   * The main entry point of the application.
+   * <p>
+   * This method provides an example of how to use the
+   * {@link AlternatingDigitSum#minimumOperations(int)} method.
+   * <p>
+   * The following example computes the alternating digit sum of the number
+   * <code>886996</code>.
+   */
   public static void main(String[] args) {
     int n = 886996;
 
